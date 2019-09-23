7b19dd9792093901bb1b26d544202cd510dba226
niko07335 <niko07335@gmail.com>
2019-09-23 16:02:56 +0300

    done 50 pass (81ms)

--- src/index.js
+++ src/index.js
@@ -38,7 +38,30 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const str = expr;
+    let arr = [];
+    const lettersCount = str.length / 10;
+    for (let i = 0; i < lettersCount; i += 1) {
+        arr.push(str.substr(10*i, 10));
+    }
+    let morseArr = arr.map( (item) => {
+        let x = [];
+        for (let i = 0; i < 10; i += 2){
+            if (item[i] === '*' ) {x.push(' '); break;}
+            else if (item[i] === '1' && item[i + 1] === '0') x.push('.');
+            else if (item[i] === '1' && item[i + 1] === '1') x.push('-');
+            else if (item[i] === '0' ) continue;
+        }
+        x = x.join('');
+
+        return x;
+    })
+    let res = [];
+    morseArr.forEach((item) =>{
+        if (item === ' ') res.push(' ');
+        else res.push( MORSE_TABLE[item]);
+    });
+    return res.join('');
 }
 
 module.exports = {
